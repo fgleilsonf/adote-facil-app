88afcdcda9484b067ec283e95b70c89d5d601065
Rayonaraa <mourarayonara546@gmail.com>
2024-08-03 10:52:02 -0300

    ajuste de conflito

--- src/data/Data.jsx
+++ src/data/Data.jsx
@@ -9,6 +9,8 @@ const LIST_PETS = [
       "Esse é um cachorrinho que estou sem condição de cuidar no momento",
     characteristics: "Comportadinho",
     size: "Pequeno",
+    image: 'https://blog.cobasi.com.br/wp-content/uploads/2020/07/boston-terrier-capa1.png'
+    
   },
   {
     name: "pet2",
@@ -19,6 +21,7 @@ const LIST_PETS = [
     description: "Esse é um gatão que estou sem condição de cuidar no momento",
     characteristics: "Calmo",
     size: "médio",
+    image: 'https://img.freepik.com/fotos-gratis/adoravel-cachorro-basenji-marrom-e-branco-sorrindo-e-dando-mais-uns-cinco-isolado-no-branco_346278-1657.jpg?size=626&ext=jpg&ga=GA1.1.1371518722.1722556800&semt=sph'
   },
   {
     name: "pet1",
@@ -30,6 +33,7 @@ const LIST_PETS = [
       "Esse é um cachorrinho que estou sem condição de cuidar no momento",
     characteristics: "Comportadinho",
     size: "pequeno",
+    image:'https://portaledicase.com/wp-content/uploads/2023/11/Gato-nomes.jpg'
   },
   {
     name: "pet2",
@@ -40,6 +44,18 @@ const LIST_PETS = [
     description: "Esse é um gatão que estou sem condição de cuidar no momento",
     characteristics: "Calmo",
     size: "médio",
+    image:'https://blog-static.petlove.com.br/wp-content/uploads/2019/06/shutterstock_632318627.jpg'
+  },
+  {
+    name: "pet5",
+    age: 21,
+    specie: "gato",
+    race: "siames",
+    gender: false,
+    description: "Esse é um gatão que estou sem condição de cuidar no momento",
+    characteristics: "Calmo",
+    size: "médio",
+    image:'https://www.patasdacasa.com.br/sites/default/files/noticias/2021/07/pitbull-8-coisas-que-voce-nao-sabia-sobre-a-raca-de-cachorro-grande.jpg'
   },
 ];
 
