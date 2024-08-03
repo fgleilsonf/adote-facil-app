55c6b8fd7f8544d39b7a327beaae7a0ed3102f50
Yuri <pedroyuri.avl@gmail.com>
2024-08-03 09:09:03 -0300

    Adicionado a Listagem dos Pets para a pagina o App

--- src/data/Data.jsx
+++ src/data/Data.jsx
@@ -1,35 +1,46 @@
-let pets = [pet1 = {
+const LIST_PETS = [
+  {
     name: "pet1",
     age: 12,
     specie: "cachorro",
     race: "vira-lata",
     gender: true,
-    discription: "Esse é um cachorrinho que estou sem condiçãp de cuidar no momento",
+    discription:
+      "Esse é um cachorrinho que estou sem condiçãp de cuidar no momento",
     characteristics: "Comportadinho",
-    size: "pequeno"
-}, pet2 = {
+    size: "pequeno",
+  },
+  {
     name: "pet2",
     age: 21,
     specie: "gato",
     race: "siames",
     gender: false,
-    discription: "Esse é um gatão que estou sem condiçãp de cuidar no momento",
+    discription: "Esse é um gatão que estou sem condição de cuidar no momento",
     characteristics: "Calmo",
-    size: "médio"}]
+    size: "médio",
+  },
+  {
+    name: "pet1",
+    age: 12,
+    specie: "cachorro",
+    race: "vira-lata",
+    gender: true,
+    discription:
+      "Esse é um cachorrinho que estou sem condiçãp de cuidar no momento",
+    characteristics: "Comportadinho",
+    size: "pequeno",
+  },
+  {
+    name: "pet2",
+    age: 21,
+    specie: "gato",
+    race: "siames",
+    gender: false,
+    discription: "Esse é um gatão que estou sem condição de cuidar no momento",
+    characteristics: "Calmo",
+    size: "médio",
+  },
+];
 
-
-
-    /*
-
-    x = {
-    name: "x",
-    age: number,
-    specie: "string",
-    race: "string",
-    gender: boolean,
-    discription: "string",
-    characteristics: "string",
-    size: "string"}
-    
-    */
-    
+export { LIST_PETS };
