38afc9bd02674cc2d21ebedcd9ee33833caf4a7b
Ramon_Silva <ramonsilvadeoliveira30@gmail.com>
2024-08-05 20:52:02 -0300

    route creation

--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,10 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
+import RoutesComponent from './routes.jsx'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <RoutesComponent/>
   </React.StrictMode>,
 )
