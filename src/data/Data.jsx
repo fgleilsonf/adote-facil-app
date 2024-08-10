33aa342aaa966215034661aa541699093f5278fe
Yuri <pedroyuri.avl@gmail.com>
2024-08-10 09:39:52 -0300

    atualização geral

--- src/data/Data.jsx
+++ src/data/Data.jsx
@@ -1,10 +1,10 @@
 const LIST_PETS = [
   {
-    name: "pet1",
-    age: 12,
+    name: "Rayonara",
+    age: 4,
     specie: "cachorro",
-    race: "vira-lata",
-    gender: true, // true for male and false for female / verdadeiro para masculino e falso para feminino
+    race: "Buldogue francês",
+    gender: false, // true for male and false for female / verdadeiro para masculino e falso para feminino
     description:
       "Esse é um cachorrinho que estou sem condição de cuidar no momento",
     characteristics: "Comportadinho",
@@ -13,46 +13,46 @@ const LIST_PETS = [
     
   },
   {
-    name: "pet2",
-    age: 21,
-    specie: "gato",
-    race: "siames",
-    gender: false,
-    description: "Esse é um gatão que estou sem condição de cuidar no momento",
+    name: "Ramon",
+    age: 4,
+    specie: "cachorro",
+    race: "vira-lata",
+    gender: true,
+    description: "Esse é um cachorrinho que estou sem condição de cuidar no momento",
     characteristics: "Calmo",
     size: "médio",
     image: 'https://img.freepik.com/fotos-gratis/adoravel-cachorro-basenji-marrom-e-branco-sorrindo-e-dando-mais-uns-cinco-isolado-no-branco_346278-1657.jpg?size=626&ext=jpg&ga=GA1.1.1371518722.1722556800&semt=sph'
   },
   {
-    name: "pet3",
-    age: 12,
-    specie: "cachorro",
-    race: "vira-lata",
+    name: "Yuri",
+    age: 1,
+    specie: "gato",
+    race: "moggie",
     gender: true,
     description:
-      "Esse é um cachorrinho que estou sem condição de cuidar no momento",
-    characteristics: "Comportadinho",
+      "Esse é um pequeno gatinho que estou sem condição de cuidar no momento",
+    characteristics: "Comportadin",
     size: "pequeno",
     image:'https://portaledicase.com/wp-content/uploads/2023/11/Gato-nomes.jpg'
   },
   {
-    name: "pet4",
-    age: 21,
+    name: "Gleison",
+    age: 2,
     specie: "gato",
-    race: "siames",
-    gender: false,
+    race: "Maine Coon",
+    gender: true,
     description: "Esse é um gatão que estou sem condição de cuidar no momento",
     characteristics: "Calmo",
     size: "médio",
     image:'https://blog-static.petlove.com.br/wp-content/uploads/2019/06/shutterstock_632318627.jpg'
   },
   {
-    name: "pet5",
-    age: 21,
-    specie: "gato",
-    race: "siames",
+    name: "Brenda",
+    age: 3,
+    specie: "cachorro",
+    race: "pit-bull",
     gender: false,
-    description: "Esse é um gatão que estou sem condição de cuidar no momento",
+    description: "Esse é um cachorrão que estou sem condição de cuidar no momento",
     characteristics: "Calmo",
     size: "médio",
     image:'https://www.patasdacasa.com.br/sites/default/files/noticias/2021/07/pitbull-8-coisas-que-voce-nao-sabia-sobre-a-raca-de-cachorro-grande.jpg'
