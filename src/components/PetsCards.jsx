20a326c57c9cdc94ee73d1ecf0251d312cb2d955
Gleilson Ferreira <fgleilsonf@gmail.com>
2024-08-10 09:52:16 -0300

    feat: added adjusted

--- src/components/PetsCards.jsx
+++ src/components/PetsCards.jsx
@@ -3,24 +3,21 @@ import Box from "@mui/material/Box";
 import Card from "@mui/material/Card";
 import CardActions from "@mui/material/CardActions";
 import CardContent from "@mui/material/CardContent";
-import Button from "@mui/material/Button";
 import Typography from "@mui/material/Typography";
-import { CardMedia } from "@mui/material";
+import {CardMedia} from "@mui/material";
 import MaleIcon from "@mui/icons-material/Male";
 import FemaleIcon from "@mui/icons-material/Female";
-import { Link } from 'react-router-dom';
 
 const bull = (
   <Box
     component="span"
-    sx={{ display: "inline-block", mx: "2px", transform: "scale(0.8)" }}
+    sx={{display: "inline-block", mx: "2px", transform: "scale(0.8)"}}
   >
     •
   </Box>
 );
 
-export default function PetsCards({ pet, onClick }) {
-  console.log("pet:", pet);
+export default function PetsCards({pet, onClick}) {
   return (
     <Card
       onClick={onClick}
@@ -33,20 +30,18 @@ export default function PetsCards({ pet, onClick }) {
         boxShadow: "0px 0px 28px #3c3c3c23",
       }}
     >
-      <CardMedia sx={{ height: 150 }} image={pet.image} title= {pet.specie} />
-
-<CardMedia
-        sx={{ height: 150 }}
+      <CardMedia
+        sx={{height: 150}}
         image={pet?.image}
         title="Cards"
       />
-      
-       
+
+
       <CardContent>
         <Typography variant="h5" component="div">
-          {pet.name} {pet.gender === true ? <MaleIcon /> : <FemaleIcon />}
+          {pet.name} {pet.gender === true ? <MaleIcon/> : <FemaleIcon/>}
         </Typography>
-        <Typography sx={{ mb: 5.0 }} color="text.secondary"></Typography>
+        <Typography sx={{mb: 5.0}} color="text.secondary"></Typography>
         <Typography variant="body2"></Typography>
       </CardContent>
       <CardActions></CardActions>
